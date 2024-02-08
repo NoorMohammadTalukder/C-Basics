80bf3dc1a8502e252ab25796e79158af48cd9399
NoorMohammadTalukder <noor11809027@gmail.com>
2024-02-09 01:37:09 +0600

    variable

---
+++ variable.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //declaration of variable
+    string name;
+    int age;
+    double bankBalanceAmount;
+    bool isStudent;
+
+    //Assignment of value
+    name="Noor Mohammad Talukder";
+    age=20;
+    bankBalanceAmount=500.2555555;
+    isStudent=true;
+
+    //print value in screen
+    cout<<"Name is:"<<name<<endl;
+    cout<<"Age:"<<age<<endl;
+    cout<<"Amount in bank in TK:"<<bankBalanceAmount<<endl;
+    cout<<"Student status:"<<isStudent<<endl;
+
+    return 0;
+}
+
+
+
+/*
+Task 1:Store below things in variables
+--name
+--marks
+--subject name
+--section (values can be a,b or c)
+
+show above variables in screen
+*/
+
+
+
